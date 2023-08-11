0a655ba4984a62f3541380e2bfa1bb50e36a5862
T.T.An <an.tranthanh@h2-inc.com>
2023-08-11 18:08:37 +0700

    add api booking

---
+++ Models/UserModel.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Text.Json.Serialization;
+
+namespace Blog.Models
+{
+    public class UserModel
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+
+        [BsonElement("username")]
+        [JsonPropertyName("username")]
+        public string username { get; set; } = null!;
+
+        [BsonElement("email")]
+        [JsonPropertyName("email")]
+        public string email { get; set; } = null!;
+
+        [BsonElement("password")]
+        [JsonPropertyName("password")]
+        public string password { get; set; } = null!;
+
+        [BsonElement("image")]
+        [JsonPropertyName("image")]
+        public string image { get; set; } = null!;
+
+        [BsonElement("verify_token")]
+        [JsonPropertyName("verify_token")]
+        public string? verify_token { get; set; } = "";
+
+        [BsonElement("verify_code")]
+        [JsonPropertyName("verify_code")]
+        public int? verify_code { get; set; } = 0!;
+
+        [BsonElement("limitLogin")]
+        [JsonPropertyName("limitLogin")]
+        public int? limitLogin { get; set; } = 0!;
+
+        [BsonElement("countLogin")]
+        [JsonPropertyName("countLogin")]
+        public int? countLogin { get; set; } = 0!;
+
+
+        [BsonElement("isAdmin")]
+        [JsonPropertyName("isAdmin")]
+        public int? isAdmin { get; set; } = 0!;
+
+        public DateTime? createdAt { get; set; } = DateTime.Now;
+        public DateTime? updatedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ utils/Email.cs
@@ -0,0 +1,36 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace Blog.utils
+{
+    public class Email
+    {
+        private readonly IConfiguration _configuration;
+
+        public Email(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task SendEmailAsync(string toEmail, string subject, string message)
+        {
+            var emailMessage = new MimeMessage();
+            emailMessage.From.Add(new MailboxAddress(_configuration["Smtp:FromName"], _configuration["Smtp:FromEmail"]));
+            emailMessage.To.Add(new MailboxAddress(toEmail));
+            emailMessage.Subject = subject;
+
+            var bodyBuilder = new BodyBuilder();
+            bodyBuilder.HtmlBody = message;
+
+            emailMessage.Body = bodyBuilder.ToMessageBody();
+
+            using (var client = new SmtpClient())
+            {
+                await client.ConnectAsync(_configuration["Smtp:Host"], int.Parse(_configuration["Smtp:Port"]), true);
+                await client.AuthenticateAsync(_configuration["Smtp:Username"], _configuration["Smtp:Password"]);
+                await client.SendAsync(emailMessage);
+                await client.DisconnectAsync(true);
+            }
+        }
+    }
+}
